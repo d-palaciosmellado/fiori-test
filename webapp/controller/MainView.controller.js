2f16325526b5ed79ce59fa4c064fcb36b0ccdaa5
d-palaciosmellado <d.palaciosmellado@zurich.com>
2025-03-19 11:21:38 +0000

    RICEF 2nd commit on development

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,10 +1,13 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
     "use strict";
 
     return Controller.extend("dpm.invoices.controller.MainView", {
         onInit() {
+            var oModel = new JSONModel();
+            this.getView().setModel(oModel, "BU");
         }
     });
 });
